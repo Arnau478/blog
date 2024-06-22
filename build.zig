73ad6b6c8579253e38152660c685b195db60e0d6
Arnau478 <arnauxabia@gmail.com>
2024-06-22 09:36:51 +0200

    Set `output_prefix` properly

--- build.zig
+++ build.zig
@@ -4,7 +4,8 @@ const zine = @import("zine");
 pub fn build(b: *std.Build) !void {
     try zine.addWebsite(b, .{
         .title = "arnau blog",
-        .host_url = "https://tilde.club/~arnau/blog",
+        .host_url = "https://tilde.club",
+        .output_prefix = "~arnau/blog",
         .layouts_dir_path = "layouts",
         .content_dir_path = "content",
         .static_dir_path = "static",
